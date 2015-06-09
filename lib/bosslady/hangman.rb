f74e6f68d9614eff7f9e7907e209ef6ed0f7af7a
James Dabbs <jamesdabbs@gmail.com>
2015-06-09 14:05:39 -0400

    Initial commit

---
+++ lib/bosslady/hangman.rb
@@ -0,0 +1,7 @@
+require "bosslady/hangman/version"
+
+module Bosslady
+  module Hangman
+    # Your code goes here...
+  end
+end
---
+++ lib/bosslady/hangman/version.rb
@@ -0,0 +1,5 @@
+module Bosslady
+  module Hangman
+    VERSION = "0.0.1"
+  end
+end
